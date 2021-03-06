905373c75095b046cbf50d16ab5e549fb4d92380
Ngoc Lam Nguyen <if19b072@technikum-wien.at>
2021-03-06 13:33:27 +0100

    added moq & logging

--- Tourplaner/TourService/Program.cs
+++ Tourplaner/TourService/Program.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace TourService
 {
@@ -13,11 +16,30 @@ namespace TourService
     {
         public static void Main(string[] args)
         {
+            StartLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+
+        private static void StartLogger()
+        {
+            var logfile = Directory.GetCurrentDirectory() + "/log.txt";
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Debug()
+                .WriteTo.Console(
+                    LogEventLevel.Verbose,
+                    "{NewLine}{Timestamp:HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}")
+                .WriteTo.File(logfile, LogEventLevel.Verbose,
+                    "{NewLine}{Timestamp:HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}",
+                    rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
     }
 }
