4d5a840dfbb69d370070806755644bc0789385d7
Ngoc Lam Nguyen <if19b072@technikum-wien.at>
2021-05-30 17:07:55 +0200

    no insert after init

--- Tourplaner/docker/db/script.sql
+++ Tourplaner/docker/db/script.sql
@@ -26,8 +26,8 @@ CREATE TABLE Log(
 	ON DELETE CASCADE
 );
 
-INSERT INTO Route(name,origin,destination,description,imagename,estimateddistance) VALUES('route1_test','origin_test','destination_test','description','placeholder.png',23.5);
-INSERT INTO Route(name,origin,destination,description,imagename,estimateddistance) VALUES('route2_test','origin2_test','destination2_test','description2','placeholder2.png',55.323);
+--INSERT INTO Route(name,origin,destination,description,imagename,estimateddistance) VALUES('route1_test','origin_test','destination_test','description','placeholder.png',23.5);
+--INSERT INTO Route(name,origin,destination,description,imagename,estimateddistance) VALUES('route2_test','origin2_test','destination2_test','description2','placeholder2.png',55.323);
 
-INSERT INTO Log(startdate,enddate,origin,destination,distance,rating,note,movementmode,mood,bpm,duration,avgspeed,kcal,route_id) VALUES(NOW()- INTERVAL '1 DAY', NOW(),'log_orgin1','log_destination1',50.0,5.4,'note1',2,2,230,INTERVAL '1 DAY',54.33,500,1);
-INSERT INTO Log(startdate,enddate,origin,destination,distance,rating,note,movementmode,mood,bpm,duration,avgspeed,kcal,route_id) VALUES(NOW()- INTERVAL '2 DAY', NOW() - INTERVAL '1 DAY','log_orgin2','log_destination2',50.0,5.4,'note1',2,2,230,INTERVAL '2 DAY',54.33,500,1);
+--INSERT INTO Log(startdate,enddate,origin,destination,distance,rating,note,movementmode,mood,bpm,duration,avgspeed,kcal,route_id) VALUES(NOW()- INTERVAL '1 DAY', NOW(),'log_orgin1','log_destination1',50.0,5.4,'note1',2,2,230,INTERVAL '1 DAY',54.33,500,1);
+--INSERT INTO Log(startdate,enddate,origin,destination,distance,rating,note,movementmode,mood,bpm,duration,avgspeed,kcal,route_id) VALUES(NOW()- INTERVAL '2 DAY', NOW() - INTERVAL '1 DAY','log_orgin2','log_destination2',50.0,5.4,'note1',2,2,230,INTERVAL '2 DAY',54.33,500,1);
